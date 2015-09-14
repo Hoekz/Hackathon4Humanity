b7422240af8af892ee302571d853c4d9edaa853d
Kiser360 <dakrkb@mst.edu>
2015-09-14 16:59:48 -0500

    More button functionality

--- app/scripts/controllers/meetings.js
+++ app/scripts/controllers/meetings.js
@@ -1,4 +1,36 @@
 app.controller('meetings', ["$scope", 'memory', function($scope, memory){
-	console.log("Loaded Meetings Controller");
+
 	$scope.isAdding = false;
+	$scope.title = "Meetings";
+	$scope.newMeetingName = "";
+	$scope.locationTypes = [{name: "school", googleId:"schoolio", approved: false},
+							{name: "park", googleId:"parkio", approved: false},
+							{name: "food", googleId:"foodio", approved: false}];
+
+	$scope.toggleAddPage = function() {
+		$scope.isAdding = !$scope.isAdding;
+		resetAddPage();
+		if($scope.isAdding) {
+			$scope.title = "Create New Meeting";
+		}
+		else {
+			$scope.title = "Meetings";
+		}
+	};
+	$scope.hideAddPage = function() {
+		$scope.isAdding = !$scope.isAdding;
+		resetAddPage();
+		$scope.title = "Meetings";
+	};
+	$scope.genUrl = function() {
+		$scope.toggleAddPage();
+	};
+
+	var resetAddPage = function() {
+		$scope.newMeetingName = "";
+		$scope.locationTypes = $scope.locationTypes.map(function(location) {
+			location.approved = false;
+			return location;
+		});
+	};
 }]);
