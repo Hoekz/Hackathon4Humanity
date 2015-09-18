af504527d94bc8a6c1c902d7685ba02177dad579
James Hoekzema <jphx48@mst.edu>
2015-09-17 19:07:53 -0500

    Added Categories for Choosing Types
    I'll work on translating those into actual query parameters in a bit.

--- app/scripts/controllers/meetings.js
+++ app/scripts/controllers/meetings.js
@@ -1,4 +1,4 @@
-app.controller('meetings', ["$scope", 'memory', function($scope, memory){
+app.controller('meetings', ["$scope", 'memory', 'categories', function($scope, memory, categories){
 	if(!memory.groups){
 		memory.groups = [];
 	}
@@ -6,9 +6,7 @@ app.controller('meetings', ["$scope", 'memory', function($scope, memory){
 	$scope.isAdding = false;
 	$scope.title = "Meetings";
 	$scope.newMeetingName = "";
-	$scope.locationTypes = [{name: "school", googleId:"schoolio", approved: false},
-							{name: "park", googleId:"parkio", approved: false},
-							{name: "food", googleId:"foodio", approved: false}];
+	$scope.locationTypes = categories;
 
 	$scope.toggleAddPage = function() {
 		$scope.isAdding = !$scope.isAdding;
---
+++ app/scripts/factories/categories.js
@@ -0,0 +1,12 @@
+app.factory('categories', function(){
+    return [
+        {name: "Food", googleId: "Cafe, Food, Meal Delivery, Meal Takeaway, Restaurant", approved: false},
+        {name: "Attractions", googleId:"Aquarium, Museum, Art Gallery", approved: false},
+        {name: "Parks", googleId:"Park, Amusement Park, Zoo", approved: false},
+        {name: "Learning", googleId:"Book Store, Library, School, University", approved: false},
+        {name:"Fashion", googleId:"Beauty Salon, Clothing Store, Jewelry Store, Shoe Store, Hair Care", approved: false},
+        {name: "Shopping", googleId:"Shopping Mall, Department Store, Convenience Store", approved: false},
+        {name: "Entertainment", googleId:"Movie Theater, Movie Rental, Stadium, Bowling Alley", approved: false},
+        {name: "Adult", googleId: "Casino, Bar, Night Club", approved: false}
+    ];
+});
